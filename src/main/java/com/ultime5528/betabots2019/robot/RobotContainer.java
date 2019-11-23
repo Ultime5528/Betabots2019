c75c4a11f13cd6822ea176ff716ae91393a6b2a0
Ultime 5528 <keranna.first@gmail.com>
2019-11-23 16:08:45 -0500

    Changement nom et désactivation commandes test

--- src/main/java/com/ultime5528/betabots2019/robot/RobotContainer.java
+++ src/main/java/com/ultime5528/betabots2019/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import com.ultime5528.betabots2019.commands.test.TournerVitesseMax;
 import com.ultime5528.betabots2019.subsystems.BasePilotableOkto;
 import com.ultime5528.betabots2019.subsystems.Ejecteur;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
@@ -17,7 +18,7 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 public class RobotContainer {
 
     private final XboxController controller = new XboxController(0);
-    private final Joystick joystick1 = new Joystick(1);
+    private final Joystick levier = new Joystick(1);
 
     private final BasePilotableOkto basePilotable = new BasePilotableOkto();
     private final Ejecteur ejecteur = new Ejecteur();
@@ -35,9 +36,9 @@ public class RobotContainer {
     public void configureBindings(){
         // TODO Erreur de compilation
         // new JoystickButton(joystick, 1).whenPressed(new Tourner(basePilotable, 90));
-        new JoystickButton(controller, 2).toggleWhenPressed(new AvancerVitesseMax(basePilotable).withTimeout(5));
-        new JoystickButton(controller, 3).toggleWhenPressed(new TournerVitesseMax(basePilotable).withTimeout(5));
-        new JoystickButton(joystick1, 1).toggleWhenPressed(new Ejecter(ejecteur));
+        //new JoystickButton(controller, 2).toggleWhenPressed(new AvancerVitesseMax(basePilotable).withTimeout(5));
+        //new JoystickButton(controller, 3).toggleWhenPressed(new TournerVitesseMax(basePilotable).withTimeout(5));
+        new JoystickButton(levier, 1).toggleWhenPressed(new Ejecter(ejecteur));
     }
 
     
